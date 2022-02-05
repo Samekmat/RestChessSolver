109d080ab4f96dc839b65f6da732dd9a5dd21272
Mateusz Samek <samekmat@gmail.com>
2022-02-05 18:56:03 +0100

    Create abstract class Figure with constructor

--- app.py
+++ app.py
@@ -9,5 +9,11 @@ def test():
     return jsonify({0: "test"})
 
 
+class Figure(ABC):
+    def __init__(self, position):
+        self.currentField = position
+        
+
+        
 if __name__ == "__main__":
     app.run(debug=True)
